d3f72d6574c8d9fed25d03f7e7c0c062acc13ee3
Thomas Sarlandie <thomas@sarlandie.net>
2016-01-31 23:43:59 -0800

    Finalizing revB
     - Added part numbers on all parts (in kicad PartNumber field)
     - Some finicking in a bunch of corners

--- hardware-v1/gerber-prep.sh
+++ hardware-v1/gerber-prep.sh
@@ -4,7 +4,8 @@ echo "Preparing files for OSHPARK"
 
 mv bb1-Ground.Plane.g2 bb1-Ground.Plane.g2l
 mv bb1-Power.g3 bb1-Power.g3l
-gerbv -x drill -o bb1-combined-drills.drl bb1.drl bb1-NPTH.drl
-rm bb1.drl bb1-NPTH.drl
+# not needed anymore. kicad 4.0.1 has an option to merge drill files
+#gerbv -x drill -o bb1-combined-drills.drl bb1.drl bb1-NPTH.drl
+#rm bb1.drl bb1-NPTH.drl
 
-zip gerber.zip bb1-B.Cu.gbl bb1-B.Mask.gbs bb1-B.SilkS.gbo bb1-combined-drills.drl bb1-Edge.Cuts.gm1 bb1-F.Cu.gtl bb1-F.Mask.gts bb1-F.SilkS.gto bb1-Ground.Plane.g2l bb1-Power.g3l bb1-F.Paste.gtp
+zip gerber.zip bb1-B.Cu.gbl bb1-B.Mask.gbs bb1-B.SilkS.gbo bb1.drl bb1-Edge.Cuts.gm1 bb1-F.Cu.gtl bb1-F.Mask.gts bb1-F.SilkS.gto bb1-Ground.Plane.g2l bb1-Power.g3l bb1-F.Paste.gtp
