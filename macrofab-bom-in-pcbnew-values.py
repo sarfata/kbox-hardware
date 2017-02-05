7e839986a38641a62c3f4644d4019d941d924be4
Thomas Sarlandie <thomas@sarlandie.net>
2017-02-04 16:05:03 -0800

    Macrofab script improvement
    Now handles DNP and substitutions

--- macrofab-bom-in-pcbnew-values.py
+++ macrofab-bom-in-pcbnew-values.py
@@ -4,9 +4,13 @@ import argparse
 import re
 import pandas
 
-def update_kicad_values(input, bom, output):
+def update_kicad_values(input, bom, output, substitutes = None):
     reference = None
     bomdf = pandas.read_csv(bom)
+
+    subdf = pandas.DataFrame()
+    if substitutes is not None:
+        subdf = pandas.read_csv(substitutes)
 
     for line in input:
         m = re.search("fp_text reference ([A-Z0-9]+)", line)
@@ -19,10 +23,21 @@ def update_kicad_values(input, bom, output):
             mpn = bomdf[bomdf["Reference"] == reference][' PartNumber']
             if len(mpn) > 0 and not pandas.isnull(mpn).values[0]:
                 mpn = mpn.values[0]
-                print("{} ==> '{}' --> {}".format(reference, value, mpn))
+
+                substitute = subdf[subdf["PartNumber"] == mpn]['Substitute']
+                if len(substitute) > 0 and not pandas.isnull(substitute).values[0]:
+                    substitute = substitute.values[0]
+                    print("{} ==> '{}' --> {} substituted by {}".format(reference, value, mpn, substitute))
+                    mpn = substitute
+                else:
+                    print("{} ==> '{}' --> {}".format(reference, value, mpn))
+
                 line = line.replace(value, mpn)
             else:
                 print("No MPN in BOM for {} ({})".format(reference, value))
+                line = line.replace(value, "DNP")
+            # Some fp_text value lines include "hide" which gets pushed to the BOM - Remove hide
+            line = line.replace("hide", "")
 
         output.write(line)
 
@@ -31,10 +46,11 @@ def main():
     parser.add_argument('input', type = argparse.FileType("r"))
     parser.add_argument('bom', type = argparse.FileType("r"))
     parser.add_argument('output', type = argparse.FileType("w"))
+    parser.add_argument('--substitute-parts', type = argparse.FileType("r"))
 
     args = parser.parse_args()
 
-    update_kicad_values(args.input, args.bom, args.output)
+    update_kicad_values(args.input, args.bom, args.output, args.substitute_parts)
 
 
 if __name__ == '__main__':
